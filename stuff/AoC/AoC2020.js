f743229cc88d502e7148bb4553b84b4738f81657
Tooster <max@polarczyk.pl>
2020-12-25 21:46:08 +0100

    Add AoC day 23 - crab cups

--- stuff/AoC/AoC2020.js
+++ stuff/AoC/AoC2020.js
@@ -48,47 +48,47 @@ function day12() {
     t = $('pre').textContent.split('\n', 755).map(ts => [ts[0], parseInt(ts.slice(1))])
 
     function rotate([x, y], dir, angle) {
-        rad = (dir == 'L' ? +1 : -1) * Math.PI * (angle/180);
+        rad = (dir == 'L' ? +1 : -1) * Math.PI * (angle / 180);
         [sina, cosa] = [Math.round(Math.sin(rad)), Math.round(Math.cos(rad))];
-        return [cosa*x - sina*y, sina*x + cosa*y];
-    }
-    
-    function translate([x, y], [dx, dy]){
-        return [x+dx, y+dy];
-    }
-    
-    function step([dir, n], [[wx, wy], [x, y]]) {
-        
-        if(dir == 'R' || dir == 'L') return [rotate([wx, wy], dir, n), [x, y]];
-        if(dir == 'F') return [[wx, wy], translate([x, y], [n*wx, n*wy])];
-    
-        [dx, dy] = {W:[-1, 0], E:[1, 0], S:[0,-1], N:[0, 1]}[dir];
-        
-        return [[wx + dx*n, wy + dy*n], [x, y]];
+        return [cosa * x - sina * y, sina * x + cosa * y];
     }
 
-    function ss([dir, n], [[wx, wy], [x, y]]){
+    function translate([x, y], [dx, dy]) {
+        return [x + dx, y + dy];
+    }
+
+    function step([dir, n], [[wx, wy], [x, y]]) {
+
+        if (dir == 'R' || dir == 'L') return [rotate([wx, wy], dir, n), [x, y]];
+        if (dir == 'F') return [[wx, wy], translate([x, y], [n * wx, n * wy])];
+
+        [dx, dy] = { W: [-1, 0], E: [1, 0], S: [0, -1], N: [0, 1] }[dir];
+
+        return [[wx + dx * n, wy + dy * n], [x, y]];
+    }
+
+    function ss([dir, n], [[wx, wy], [x, y]]) {
         s2 = step([dir, n], [[wx, wy], [x, y]]);
         console.log(dir, n, [wx, wy], [x, y], `-->`, s2[0], s2[1]);
         return s2;
     }
 
     state = [[10, 1], [0, 0]]
-    for(let i =0; i<t.length; ++i) state = step(t[i], state);
+    for (let i = 0; i < t.length; ++i) state = step(t[i], state);
 }
 
-function day13(){
+function day13() {
     // 1. modulo each and minimum 
     // 2. chinese remainder theorem + wolfram: 
 }
 
-function day15(){
-    t = [2,0,6,12,1,3];
-    at = {2:1, 0:2, 6:3, 12:4, 1:5};
+function day15() {
+    t = [2, 0, 6, 12, 1, 3];
+    at = { 2: 1, 0: 2, 6: 3, 12: 4, 1: 5 };
     let [last, i] = [3, 6]
     // d = t.slice(0, -1);
-    for(;i<30000000;++i){
-    //     d = [...d, last]
+    for (; i < 30000000; ++i) {
+        //     d = [...d, last]
         let seenAt = at[last];
         at[last] = i;
         last = seenAt ? i - seenAt : 0;
@@ -96,3 +96,55 @@ function day15(){
     // d
     [i, last]
 }
+
+function day23() {
+
+    t = [1, 5, 7, 6, 2, 3, 9, 8, 4]
+
+    function buildChain(t) {
+        let d = t.map((v, i) => Object.assign({}, { v: v }));
+        let f = d[0];
+        d.forEach((o, i) => o.n = (d[i + 1] || d[0])); // assign place successor
+        d.sort((o1, o2) => o2.v - o1.v);
+        d.forEach((o, i) => o.vn = (d[i + 1] || d[0])); // assign value successor
+        for (const i in t) [d[i], f] = [f, f.n];
+        return d;
+    }
+
+    function turn(cur) { // current is at last idx
+        let three = [cur.n, cur.n.n, cur.n.n.n];
+        let [x, y, z] = three;
+        cur.n = z.n; // cut the 3 out the chain
+        let dst = cur;
+        do { dst = dst.vn; } while (three.includes(dst)); // find next value-wise not in picked up
+        z.n = dst.n;
+        dst.n = x;
+        return cur.n;
+    }
+
+    function printChain(start) {
+        let s = '';
+        let cur = start;
+        do { s += cur.v; cur = cur.n; } while (cur != start);
+        return s;
+    }
+
+    function sim(t, lim=100) {
+        let d = buildChain(t);
+        let one = d.find(o => o.v == 1);
+        let cur = one;
+        for (let i = 0; i < lim; ++i) cur = turn(cur);
+        // console.log(printChain(one));
+        return one;
+    }
+
+    // part 1.
+    let p1 = sim(t); // gotta take what is AFTER number 1 only (8 digits)
+    console.log(printChain(p1).substr(1));
+
+    // part 2.
+    let p2 = sim([...t, ...Array.from(new Array(1000000),(_,i)=>i+1).splice(9)], 10000000) 
+    console.log(p2.n.v * p2.n.n.v);
+}
+
+day23();
