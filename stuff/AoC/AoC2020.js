190dcf41e9ad6484cd9fe93a60e59df248d4fca5
Tooster <max@polarczyk.pl>
2020-12-26 23:39:30 +0100

    Add day 24 solution to AoC combolist

--- stuff/AoC/AoC2020.js
+++ stuff/AoC/AoC2020.js
@@ -147,4 +147,30 @@ function day23() {
     console.log(p2.n.v * p2.n.n.v);
 }
 
-day23();
+function day24(){
+    t = $('pre').textContent.split('\n').filter(ts => ts.length>0)
+    function instructionWalk(instr, cord=[0,0]) {
+        while(instr.length > 0){
+            let m = ["e", "w", "ne", "nw", "se", "sw"].find(m => instr.indexOf(m) == 0);
+            instr = instr.substr(m.length);
+            cord = move(cord, m);
+        }
+        return cord;
+    }
+    tiles = t.map(instr => instructionWalk(instr))
+    console.log(Object.values(tiles.reduce((o, cords) => Object.assign(o, {[cords]:!o[cords]}), {})).filter(v => v).length) // part 1
+
+    life = Object.entries(tiles.reduce((o, cords) => Object.assign(o, {[cords]:!o[cords]}), {})).filter(([k, v]) => v).map(a => a[0].split(',').map(x => +x))
+    startPopulation = life.reduce((o, c) => Object.assign(o, {[c]:true}), {});
+    function relax(life){
+        let toCords = (c => c.split(',').map(x=>+x));
+        let cords = Object.keys(life).map(toCords).map(c => [c, ...['e', 'w', 'ne', 'nw', 'se', 'sw'].map(dd => instructionWalk(dd, c))]).flat().reduce((o, c) => Object.assign(o, {[c]:true}), {});
+        return Object.keys(cords).map(toCords).filter(c => {
+            let neigh = ['e', 'w', 'ne', 'nw', 'se', 'sw'].map(dd => instructionWalk(dd, c)).filter(v=>life[v]).length; // count neighbors
+            return  neigh == 2 || life[c] && neigh == 1;
+        }).reduce((o, c) => Object.assign(o, {[c]:true}), {});
+    }
+    let p = startPopulation;
+    for(let i=0; i<100; ++i) p = relax(p);
+    console.log(Object.values(p).length);
+}
