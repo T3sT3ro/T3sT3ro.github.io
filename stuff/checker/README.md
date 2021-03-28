1a86e5c8630cbda63741f04d0a9b534b9b1ccd7d
Tooster <max@polarczyk.pl>
2021-03-28 20:31:44 +0200

    enchance checker.js, add README.md to it

---
+++ stuff/checker/README.md
@@ -0,0 +1,54 @@
+# Checker
+
+## What's that ?
+
+This small Node.JS script is used to test a program agains test cases. Program is supplied by `*.in` files to the `stdin` and it's `stdout` is compared agains `*.out` files. It automates and presents nicely the results of running the tests.
+
+To have even nicer output I recommend using [my formatter](https://github.com/T3sT3ro/T3sT3ro.github.io/tree/master/stuff/formatter) by piping the checker to interactive formatter process without buffering:
+
+```
+check prog test c: | stdbuf -i0 -o0 formatter
+```
+
+## Use cases
+
+1. Learn more about checker and options
+
+   ```
+   node check ?
+   ```
+
+2. Test program `prog` for all `*.in` files in directory `tests` and compare outputs with corresponding `*.out` files:
+
+    ```
+    node check prog tests
+    ```
+
+3. Test program `prog` agains tests `A` and `B` in directory `tests` and show diff for tests that differ
+
+    ```
+    node check showDiff: prog tests A B 
+    ```
+
+4. Generate outputs `solutions/*.out.tee` for bruteforce program `brute` for all inputs `*.in` in directory `tests`
+
+    ```
+    node check noCheck: tee:solutions brute tests
+    ```
+
+    or
+
+    ```
+    node check gen:solutions brute tests
+    ```
+
+5. Test program `prog` for `*.in` files agains `*.out` in directory `tests`, show colorful status report in terminal with `c:` and piping to `formatter`, show command used to launch each test case with `showCmd:`, store outputs from program to `*.out.tee` files in the same directory `tests` with `tee:` and `diff` only with `-b` flag for tests (assuming that my custom `formatter` is installed)
+
+    ```
+    node check color: tee: showCmd: diff: diffFlags:b prog tests | stdbuf -i0 -o0 formatter
+    ```
+
+## TODO
+
+- [ ] Add support for validator instead of `*.out` files
+- [ ] Support timeout to exec function in some `tests.lim` file as map `testName: time limit for timeout` or make each file start with some header specifying test parameters
---
+++ stuff/checker/check.js
@@ -0,0 +1,146 @@
+const fs = require('fs');
+const proc = require('child_process');
+const path = require('path');
+
+
+let usage = `
+usage: node checker.js [opts...] <program> <test_dir> [tests...]
+    use '?' or 'help:' for more help
+`.trim()
+let help = `
+- tests' filename format is '*.in' and '*.out'
+- optional arguments are in format 'optName:optVal'
+- to run only selected tests list them by their basename e.g. A B C
+- it's good to run 'find . -type f -exec sed -i -e '$a\' {} \; -print' for generated tests to append newlines
+
+optional arguments: (opt:* means that opt accepts value)
+    ?       help:       - prints this help
+    c:      color:      - wrap in formatter coloring codes (piping to formatter via 'stdbuf -i0 -o0 formatter' is best)
+    nc:     noCheck:    - don't validate outputs agains *.out files just run the program
+    d:      showDiff:   - when test ends with ERROR show additional diff log.
+    df:*    diffFlags:* - provide custom flags for diff - by default only 'Z' is used. leave empty to disable Z
+    sc:     showCmd:    - print commands used for each test case
+            program:*   - explicit program name - positional argument ignored
+            tests:*     - explicit directory with tests - positional argument ignored
+            tee:*       - generates *.out.tee files from *.in files to specified directory (by default same as tests).
+            gen:*       - same as combination 'tee:* noCheck:' - used to generate outputs
+`
+
+{ // argument line parsing
+    var args = process.argv.slice(2);
+    var positional = args.filter(arg => !arg.includes(':'));
+    var opts = args.filter(arg => arg.includes(':'))
+        .map(it => it.split(':'))
+        .reduce((opts, [k, v]) => Object.assign(opts, { [k]: v || true }), {});
+    let pos = 0 // current positional - to allow mixing positional and arguments
+    var program = opts.program || positional[pos++];
+    var testDir = opts.tests || positional[pos++];
+    var requestedTests = positional.slice(pos);
+}
+
+// escapes whitespace in path name with '\ '
+function escapeWS(path) { return path.replace(/(\s+)/g, '\\$1') }
+
+{ // bad args and usage 
+    let programExists = fs.existsSync(program);
+    let testsExist = fs.existsSync(testDir);
+    let printHelp = args.find(it => it.match(/\?/)) || opts.help;
+
+    if (!programExists || !testsExist || printHelp) {
+        if (!printHelp && !programExists) console.error(`can't find program '${program}' in cwd: '${__dirname}'`);
+        if (!printHelp && !testsExist) console.error(`can't find tests directory '${testDir}' in cwd: '${__dirname}'`);
+        console.error(usage);
+        if (printHelp) console.error(help);
+        process.exit(printHelp ? 0 : 1);
+    }
+}
+
+{ // option acronym expansion
+    let expand = (acronym, long) => { opts[long] = opts[long] || opts[acronym]; delete opts[acronym] };
+    expand("c", "color");
+    expand("nc", "noCheck");
+    expand("d", "showDiff");
+    expand("df", "diffFlags");
+    expand("sc", "showCmd");
+}
+
+{ // default arguments
+    if (opts.gen) {
+        opts.tee = opts.gen;
+        opts.noCheck = true;
+    }
+    if (opts.diffFlags === true) opts.diffFlags = '' // flags zeroed by `diff:`
+    else if (!opts.diffFlags) opts.diffFlags = '-Z' // default diff flag set to -Z (ignore trailing whitespaces)
+    else opts.diffFlags = "-" + opts.diffFlags
+
+    if (opts.tee === true) opts.tee = testDir
+    if (opts.tee)
+        fs.mkdirSync(escapeWS(opts.tee), { recursive: true });
+}
+
+
+// wraps string in format if opts.color is specified 
+// for my formatter see https://github.com/T3sT3ro/T3sT3ro.github.io/tree/master/stuff/formatter
+function fmt(format, str) { return opts.color ? `{${format}--${str}--}` : str }
+
+console.log(fmt("*y", "PROGRAM: ") + fmt("Y/", program));
+console.log(fmt("*y", "TESTDIR: ") + fmt("Y/", testDir));
+
+let files = fs.readdirSync(testDir);
+
+let ins = files.filter(f => f.match(/.*\.in$/));
+let outs = files.filter(f => f.match(/.*\.out$/));
+
+let insNames = ins.map(it => it.replace('.in', ''));
+let outsNames = outs.map(it => it.replace('.out', ''));
+
+// orded tests by named first, numbered later
+let tests = insNames
+    .filter(it => outsNames.includes(it))
+    .sort((a, b) => a.localeCompare(b, undefined, { numeric: true, sensitivity: 'base' }));
+tests = [...tests.filter(t => !t.match(/\d+/)), ...tests.filter(t => t.match(/\d+/))];
+
+if (requestedTests.length > 0)
+    tests = requestedTests.filter(test => tests.includes(test));
+
+let failedTests = []
+for (const test of tests) {
+
+    let testFile = path.join(testDir, test);
+    let teeCmd = opts.tee ? `| tee ${escapeWS(path.join(opts.tee, `${test}.out.tee`))}` : "";
+    let diffCmd = opts.noCheck ? "" : `| diff ${opts.diffFlags} - ${escapeWS(testFile)}.out`
+
+    // sed is used to append missing newlines to input - because for example bash can't handle with while read files without NL
+    let cmd = `cat ${escapeWS(testFile)}.in | sed '$a\\' | ./${escapeWS(program)} ${teeCmd} ${diffCmd}`;
+
+    process.stdout.write(`${fmt("b*", "RUNNING")}: ${test}... `);
+    try {
+        status = proc.execSync(cmd, { stdio: 'pipe' });
+        process.stdout.write(fmt("g", ("OK" + (opts.noCheck ? "(NOCHECK)" : "")).padEnd(14)) + (opts.showCmd ? `$ ${cmd}\n` : "\n"));
+    } catch (err) {
+        failedTests.push(test);
+        process.stdout.write(fmt("r", "ERROR".padEnd(14)) + (opts.showCmd || err.stderr.length > 0 ? `$ ${cmd}\n` : "\n"));
+        if (err.stderr.length > 0) {
+            process.stdout.write(String.fromCharCode.apply(null, err.stderr));
+        }
+        if (opts.showDiff) {
+            let diff = String.fromCharCode.apply(null, err.stdout)
+                .replace(/^(.*)/, fmt("/c", "$1"))
+                .replace(/^< (.*)$/gm, fmt("r/", `RETURNED: ${fmt("/%", "$1")}`))
+                .replace(/^> (.*)$/gm, fmt("g/", `EXPEXTED: ${fmt("/%", "$1")}`))
+                .replace(/^/gm, ' ')
+            console.log(diff);
+        }
+    }
+
+}
+
+let exitCode = 0
+if (failedTests.length == 0) {
+    console.log(fmt("*g", `ALL ${tests.length}/${tests.length} TESTS PASSED` + (opts.noCheck ? " (NOCHECK MODE)" : "")))
+} else {
+    console.log(fmt("*r", `${failedTests.length}/${tests.length} TESTS PASSED. TESTS THAT FAILED:\n` +
+        failedTests.map(t => escapeWS(t)).join(" ")))
+}
+
+process.exit(exitCode)
