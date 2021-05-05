56408d3c8933ce9bb6ccabad165f7441bc955e8e
Tooster <max@polarczyk.pl>
2021-05-06 00:11:48 +0200

    CF - add 1520E, update check, add .gitignore

--- algo/cpp/CFs/1520/1520E.cpp
+++ algo/cpp/CFs/1520/1520E.cpp
@@ -2,9 +2,45 @@
 
 using namespace std;
 
+#define N 1000006
+#define long int64_t
+
+vector<long> pref(N, 0);
+vector<long> suff(N, 0);
+int         n;
+string      s;
+
+template<typename T>
+void traverse(int startInclusive, int endExclusive, int delta, vector<T> &fold) {
+    for (int i = startInclusive, sheep = 0; i != endExclusive; i += delta) {
+        if (s[i] == '.' && sheep == 0) continue;
+
+        if (s[i] == '*') {
+            ++sheep;
+            fold[i] = i > 0 ? fold[i - delta] : 0;
+        } else {
+            fold[i] = (i > 0 ? fold[i - delta] : 0) + sheep;
+        }
+    }
+}
+
 void doTest() {
-    int n;
     cin >> n;
+    cin >> s;
+
+    {  // init, innit ?
+        pref.assign(s.size()+1, 0);
+        suff.assign(s.size()+1  , 0);
+    }
+
+    traverse(0, s.size(), +1, pref);
+    traverse(s.size() - 1, -1, -1, suff);
+
+    long moves = LLONG_MAX;
+    for (int i = 0; i < n - 1; i++)
+        moves = min(moves, pref[i] + suff[i + 1]);
+
+    cout << (moves == LLONG_MAX ? 0 : moves) << "\n";
 }
 
 int main() {
