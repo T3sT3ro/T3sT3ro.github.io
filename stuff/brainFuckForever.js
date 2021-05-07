68aea5cc276633998a15230f14b7413359da5788
Tooster <max@polarczyk.pl>
2021-05-07 11:33:23 +0200

    Bff - hehe, add coroutines to brain fuck

--- stuff/brainFuckForever.js
+++ stuff/brainFuckForever.js
@@ -1,124 +1,184 @@
-// brainfuck extended: https://esolangs.org/wiki/Extended_Brainfuck
-// some things were changed
-//   special [op] memory cell is added
-//   instead of basic ascii, I use Unicode
-//   X - e[X]change [rip, instr] := [rsp, mem]
-//   @ - end the program - rip stops at this cell and doesn't advance
-//   & - mem[rsp] := rsp - store pointer address in current cell
-//   : - rsp := rsp + mem[rsp] - move head
-//   ; - [op, mem[rsp]] := [mem[rsp], op] - swap op and memory
-//   ? - goto absolute - rip := mem[rsp]
+// BrainFuckForever - interpretation of brainfuck extension by me: https://esolangs.org/wiki/Extended_Brainfuck
+// some things were changed deliberately
+//   All tapes are unbound in both directions.
+//   Memory tape is unique to timeline
+//   Register memory [R] holds registers, has pointer R to current register and is shared among timelines
+//   Instead of basic ascii, I use Unicode.
+//   new timelines are spawned as last
+//
+// naming conventions:
+//   RIP   - instruction pointer
+//   RSP   - tape pointer
+//   R     - registers tape (shared memory)
+//   r     - register pointer (private register pointer)
+//   MEM   - memory tape (timeline specific)
+//   INSTR - instruction tape (private)
+//   TID   - timeline ID. Base timeline AKA init has TID=0
+//   T     - all timelines/processes
+//
+// instructions:
+//   <>+-.,[] - standard brainfuck set
+//   ?        - goto absolute - rip := R[r] (mnemonic: where to now?)
+//   {}       - similar to [] but condition is flipped (enter if mem[rsp] == 0)
+//   ()       - move register pointer r one left/right. create and initialize register to 0 if needed
+//   ^        - r := mem[rsp] - sets register pointer to value from memory (mnemonic: arrow pointing at register)
+//   #        - mem[rsp] := r - copies the register number to memory (mnemonic: query current cell)
+//   !        - mem[rsp] := R[r] - copy current register to memory (mnemonic: looks like meteor falling from register to memory)
+//   $        - R[r] := mem[rsp] - copy memory to current register (mnemonic: set $variable in register)
+//   s        - [R[r], mem[rsp]] := [mem[rsp], R[r]] - swap current register content and memory cell (mnemonic: [s]wap)
+//   &        - R[r] := rsp - store memory pointer address in current register (mnemonic: reference like in C)
+//   :        - rsp := rsp + R[r] - move head by offset in current register (mnemonic: like move cursor in vim)
+//   @        - rsp := R[r] - move head absolute (mnemonic: place head AT)
+//   =_*/%    - math operators - R[r] := R[r] +-*/ mem[rsp] (mnemonic: just look at the keyboard...)
+//   X        - e[X]change [rip, instr] := [rsp, mem] - swaps instruction memory, data and pointers (mnemonic: e[X]change)
+//   Y        - fork the process copying the memory. write TID to mem[rsp] in parent and 0 in child. (mnemonic: forking path)
+//   |        - yield this timeline. Go to T[TID+R[r]] or continue if doesn't exist(mnemonic: flat stop sign on road)
+//   ~        - query TID of process mem[rsp] =: TID (mnemonic: wave as a timeline)
+//   ;        - end the program - kills the timeline. If no timeline's left, nothing further evaluates. (mnemonic: semicolon...)
+//   '        - toggle tokenization mode. when ON - write mem[rsp] := instr[rip].codePoint and move rsp after each one
+//   "        - mem[rsp] := `'` aka 39 (mnemonic: looks like escaped single quote by doubling it :)
 //   [0-9a-f] - write hexadecimal constant to mem[rsp]
-//   =_*/ - math operators - mem[rsp] := mem[rsp] +-*/ [op]
-//   $ - [op] := mem[rsp] 
-//   ! - mem[rsp] := [op]
-
-
-function sanitize(code) { return code.split('').filter(c => c.match(/[-+><\[\]\.,@?!$;&:*\/_=%X0-9a-f]/)).join(''); }
-
-function bracket(xs, i, br = '[]') { // finds position of matching bracket
-    let sd = { [br[0]]: 1, [br[1]]: -1 }
-    let [step, ss] = [sd[xs[i]], sd[xs[i]] || 0];
-    do[i, ss] = [i + step, ss + (sd[xs[i + step]] || 0)]; while (ss != 0 && i >= 0 && i < xs.length);
-    return i
-}
 
 if (Object.fromEntries == undefined) Object.fromEntries = es => es.reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {})
 
-function fromCodePoint(v) { return String.fromCodePoint(Math.max(0, Math.min(v, 0x10FFFF))); }
 
-function codepointRemap(obj) { // maps values to/from codePoints. Compacts into array if can. non-cedepoints are fixed points
-    return Object.assign([], Object.fromEntries(
-        Object.entries(obj).map(([k, v]) => {
-            if ((v < 0 || v > 0x10FFFF)) return [k, v];
-            if (typeof (v) == 'number') return [k, String.fromCodePoint(v)];
-            return [k, v.codePointAt()];
-        }))
-    );
+class Bff {
+
+    constructor(initialState) {
+        this.tid = initialState?.tid || 0;
+        this.rip = initialState?.rip || 0;
+        this.rsp = initialState?.rsp || 0;
+        this.mem = initialState?.mem?.slice() || [0];
+        this.r = initialState?.r || 0;
+        this.instr = initialState?.instr || '';
+        this.io = initialState.io || {};
+        this.io.i = initialState?.io?.i || '';
+        this.io.o = initialState?.io?.o || '';
+        this.T = initialState?.T?.slice() || [this];
+        this.R = initialState?.R?.slice() || [0];
+        this.tokenize = initialState.tokenize || false;
+        this.time = initialState.time || 0;
+    }
+
+    static #bracket(xs, i, br = '[]') { // finds position of matching bracket
+        let sd = { [br[0]]: 1, [br[1]]: -1 }
+        let [step, ss] = [sd[xs[i]], sd[xs[i]] || 0];
+        do[i, ss] = [i + step, ss + (sd[xs[i + step]] || 0)]; while (ss != 0 && i >= 0 && i < xs.length);
+        return i
+    }
+
+    static #fromCodePoint(v) { return String.fromCodePoint(Math.max(0, Math.min(v, 0x10FFFF))); }
+
+    // maps values to/from codePoints. Compacts into array if can. non-codePoints are fixed points
+    static #recode(obj) {
+        return Object.assign([], Object.fromEntries(
+            Object.entries(obj).map(([k, v]) => {
+                if ((v < 0 || v > 0x10FFFF)) return [k, v];
+                if (typeof (v) == 'number') return [k, String.fromCodePoint(v)];
+                return [k, v.codePointAt()];
+            }))
+        );
+    }
+
+    // static getTape(tape) { return Object.entries(tape).sort((c1, c2) => +c1[0] - +c2[0]).map(([k, v]) => v); }
+
+    get finished() { return !this.tokenize && this.instr[this.rip] == ';' || this.rip < 0 || this.rip >= this.instr.length }
+
+    tick() {
+        if (this.finished) return this;
+        ++this.time;
+        let c = this.instr[this.rip];
+        if (c == `'`) {
+            ++this.rip;
+            this.tokenize = !this.tokenize;
+            return this;
+        }
+        if (this.tokenize) {
+            ++this.rip;
+            this.mem[this.rsp] = c.codePointAt();
+            this.mem[++this.rsp] = this.mem[this.rsp] || 0;
+            return this;
+        }
+
+        switch (c) {
+            // vanilla
+            case `+`: ++this.rip; this.mem[this.rsp]++; break;
+            case `-`: ++this.rip; this.mem[this.rsp]++; break;
+            case `<`: ++this.rip; this.mem[--this.rsp] = this.mem[this.rsp] || 0; break;
+            case `>`: ++this.rip; this.mem[++this.rsp] = this.mem[this.rsp] || 0; break;
+            case `[`: this.rip = this.mem[this.rsp] != 0 ? this.rip + 1 : Bff.#bracket(this.instr, this.rip) + 1; break;
+            case `]`: this.rip = Bff.#bracket(this.instr, this.rip); break;
+            case `,`: ++this.rip; this.mem[this.rsp] = this.io.i.codePointAt() || 0; this.io.i = this.io.i.substr(1); break;
+            case `.`: ++this.rip; this.io.o = this.io.o.concat(Bff.#fromCodePoint(this.mem[this.rsp])); break;
+            // extended control flow
+            case `?`: this.rip = this.R[this.r]; break;
+            case `{`: this.rip = this.mem[this.rsp] == 0 ? this.rip + 1 : Bff.#bracket(this.instr, this.rip, '{}') + 1; break;
+            case `}`: this.rip = Bff.#bracket(this.instr, this.rip, '{}'); break;
+            // register control
+            case `(`: ++this.rip; this.R[--this.r] = this.R[this.r] || 0; break;
+            case `)`: ++this.rip; this.R[++this.r] = this.R[this.r] || 0; break;
+            case `^`: ++this.rip; this.r = this.mem[this.rsp]; break;
+            case `#`: ++this.rip; this.mem[this.rsp] = this.r; break;
+            // register <-> memory operations
+            case `!`: ++this.rip; this.mem[this.rsp] = this.R[this.r]; break;
+            case `$`: ++this.rip; this.R[this.r] = this.mem[this.rsp]; break;
+            case `s`: ++this.rip;[this.R[this.r], this.mem[this.rsp]] = [this.mem[this.rsp], this.R[this.r]]; break;
+            case `&`: ++this.rip; this.R[this.r] = this.rsp; break;
+            case `:`: ++this.rip; this.mem[this.rsp += this.R[this.r]] = this.mem[this.rsp] || 0; break;
+            case `@`: ++this.rip; this.mem[this.rsp = this.R[this.r]] = this.mem[this.rsp] || 0; break;
+            // arithmetic
+            case `*`: ++this.rip; this.R[this.r] = this.R[this.r] * this.mem[this.rsp]; break;
+            case `/`: ++this.rip; this.R[this.r] = Math.floor(this.R[this.r] / this.mem[this.rsp]); break;
+            case `=`: ++this.rip; this.R[this.r] = this.R[this.r] + this.mem[this.rsp]; break;
+            case `_`: ++this.rip; this.R[this.r] = this.R[this.r] - this.mem[this.rsp]; break;
+            case `%`: ++this.rip; this.R[this.r] = this.R[this.r] % this.mem[this.rsp]; break;
+            // w^x            
+            case `X`:
+                [this.rip, this.rsp] = [this.rsp, this.rip];
+                [this.mem, this.instr] = [Bff.#recode(this.instr), Bff.#recode(this.mem)]; break;
+            // timelines
+            case `Y`:
+                ++this.rip;
+                this.T.push(Object.assign(new Bff(this),
+                    { tid: this.T.length, mem: Object.assign(this.mem.slice(), { [this.rsp]: 0 }), }));
+                this.mem[this.rsp] = this.T[this.T.length - 1].tid;
+                return this;
+            case `|`: ++this.rip; return this.T[this.R[this.r]] || this;
+            case `~`: ++this.rip; this.R[this.r] = this.tid; break;
+
+            // stop
+            case `;`: return this;
+            // constants
+            case `"`: ++this.rip; this.mem[this.rsp] = '"'.codePointAt(); this.mem[++this.rsp] = this.mem[this.rsp] || 0;
+            default: ++this.rip; if (c?.match(/[0-9a-f]/)) this.mem[this.rsp] = parseInt('0x' + c);
+        }
+
+        return this;
+    }
+
+    run(opts) {
+        let { tickLim, onData } = Object.assign({ tickLim: 100000, onData: IO => { } }, opts);
+        let s = this;
+        while (s.time != tickLim && !s.finished) {
+            s = s.tick();
+            if (s.io.o != '')
+                onData(s.io);
+        }
+        onData(s.io); return s;
+    }
 }
 
-class BFF {
-    constructor(other) {
-        this.instr = other.instr || '';
-        this.rip = other.rip || 0;
-        this.rsp = other.rsp || 0;
-        this.op = other.op || 0;
-        this.mem = Object.assign([0], other.mem);
-        this.io = Object.assign({ i: '', o: '' }, other.io);
-    }
+if (process.argv[2] == 'test') {
+    let ioQuine = new Bff({ instr: ",[>,]@X", io: { i: ">>@[.>]" } });
+    let fibonacci = new Bff({ instr: "0>1[$< = >>!]" });
+    let quine = new Bff({ instr: "'>@[.>];'@X" });
+    let fork = new Bff({ instr: "Y${'World!'f}!['Hello ']0)@[.>](![|]" });
 
-    get isTerminal() { return this.rip < 0 || this.rip >= this.instr.length || this.instr[this.rip] == '@'; }
+    let vmOstream = (IO) => { process.stdout.write(IO.o == '' ? '\n' : IO.o); IO.o = ''; };
 
-    get memoryArray() { return Object.entries(this.mem).sort((c1, c2) => parseInt(c1[0]) - parseInt(c2[0])).map(([k, v]) => v); }
-
-    succ() {
-        let s = new BFF(this);
-        let { instr, rip, rsp, op, mem, io } = s;
-
-        if (this.isTerminal) return s;
-
-        return Object.assign(s, (c => {
-            switch (c) {
-                case '+': return { rip: rip + 1, mem: Object.assign(mem, { [rsp]: mem[rsp] + 1 }) };
-                case '-': return { rip: rip + 1, mem: Object.assign(mem, { [rsp]: mem[rsp] - 1 }) };
-                case '<': return { rip: rip + 1, rsp: rsp - 1, mem: Object.assign({ [rsp - 1]: 0 }, mem) };
-                case '>': return { rip: rip + 1, rsp: rsp + 1, mem: Object.assign({ [rsp + 1]: 0 }, mem) };
-                case '[': return { rip: mem[rsp] != 0 ? rip + 1 : bracket(this.instr, rip) + 1 };
-                case ']': return { rip: bracket(instr, rip) };
-                case ',': return { rip: rip + 1, mem: Object.assign(mem, { [rsp]: io.i.codePointAt() || 0 }), io: { i: io.i.substr(1), o: io.o } };
-                case '.': return { rip: rip + 1, io: { i: io.i, o: io.o + fromCodePoint(mem[rsp]) } };
-
-                case '?': return { rip: mem[rsp] };
-
-                case '!': return { rip: rip + 1, mem: Object.assign(mem, { [rsp]: op }) };
-                case '$': return { rip: rip + 1, op: mem[rsp] };
-                case ';': return { rip: rip + 1, op: mem[rsp], mem: Object.assign(mem, { [rsp]: op }) }
-
-                case '&': return { rip: rip + 1, mem: Object.assign(mem, { [rsp]: rsp }) };
-                case ':': return { rip: rip + 1, rsp: rsp + mem[rsp], mem: Object.assign({ [rsp + mem[rsp]]: 0 }, mem) };
-
-                case '*': return { rip: rip + 1, mem: Object.assign(mem, { [rsp]: mem[rsp] * op }) }
-                case '/': return { rip: rip + 1, mem: Object.assign(mem, { [rsp]: Math.floor(mem[rsp] / op) }) }
-                case '=': return { rip: rip + 1, mem: Object.assign(mem, { [rsp]: mem[rsp] + op }) }
-                case '_': return { rip: rip + 1, mem: Object.assign(mem, { [rsp]: mem[rsp] - op }) }
-                case '%': return { rip: rip + 1, mem: Object.assign(mem, { [rsp]: mem[rsp] % op }) }
-
-                case 'X': return { rip: rsp, rsp: rip, mem: codepointRemap(instr), instr: codepointRemap(mem) };
-
-                default: return `${c}`.match(/[0-9a-f]/) ?
-                    { rip: rip + 1, mem: Object.assign(mem, { [rsp]: parseInt('0x' + c) }) } :
-                    { rip: rip + 1 };
-            }
-        })(this.instr[this.rip]));
-    }
-
-    eval(reps = 1000) {
-        let s = this
-        while (reps--) { s = s.succ(); }
-        return s;
-    }
-
-    get pretty() {
-        return `
-context: ... ${[-4, -3, -2, -1, 0, 1, 2, 3, -4].map(d => this.instr[this.rip+d]).join('')} ...
-                 ^ [rip:${this.rip}]
-...
-[${this.rsp-2}] ->   ${this.mem[this.rsp-2]}
- [${this.rsp-1}] ->  ${this.mem[this.rsp-1]}
-  [${this.rsp+0}] -> ${this.mem[this.rsp]}
- [${this.rsp+1}] ->  ${this.mem[this.rsp+1]}
-[${this.rsp+2}] ->   ${this.mem[this.rsp+2]}
-...
-[op:${this.op}]
-
-[in : ${this.io.i}]
-[out: ${this.io.o}]`;
-    }
+    ioQuine.run({ onData: vmOstream }); // self printer with IO
+    console.log(fibonacci.run({ tickLim: 3 + 8 * 20 }).mem);
+    quine.run({ onData: vmOstream });
+    fork.run({ onData: vmOstream });
 }
-
-console.log((new BFF({ instr: ",[>,]&;_:X", io: { i: ">>&;0_:[.>]" } })).eval().io.o); // self printer with IO
-
-console.log((new BFF({ instr: "0>1[<$>>=<$>=]" })).eval().pretty);
-
-// working quine
-console.log((new BFF({ instr: "f;4*;2=> f;4*;2=> f;3*;3=> f;3*;e=> f;2*;8=> f;3*;e=> f;6*;5=> f;3*;d=> f;6*;1=> f;3*;1=> f;4*;2=> f;6*;3=> f;4*;4=> >0;&;_:X"})).eval().io.o);
+module.exports = Bff;
