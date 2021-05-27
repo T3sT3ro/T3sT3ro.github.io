9990a288e45f00bab738ef16403de2c60caf32d1
Tooster <max@polarczyk.pl>
2021-05-27 11:11:13 +0200

    add centroid JS implementation

---
+++ algo/algorithms/centroid.js
@@ -0,0 +1,19 @@
+function centroid(G) { // G is graph
+    let n = G.size;
+    let [centroids, sz] = [[], []]; // sz = sizes of subtrees
+            
+    function dfs(u, prev) {
+        sz[u] = 1;
+        let centroidCandidate = true;
+        for (const v of G[u]) if (v != prev) {
+            dfs(v, u);
+            sz[u] += sz[v];
+            if (sz[v] > n / 2) centroidCandidate = false;
+        }
+        if (n - sz[u] > n / 2) centroidCandidate = false;
+        if (centroidCandidate) centroids.push(u);
+    }
+    
+    dfs(0, -1);
+    return centroids;
+}
