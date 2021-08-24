964561eec07ed27e82d26f7734d98222ec8fa1ad
Tooster <max@polarczyk.pl>
2021-08-24 09:34:43 +0200

    Add 2D kotlin utils, update gemfile, add scrach ipynbs

---
+++ algo/kotlin/src/main/kotlin/me/tooster/util/Extensions.kt
@@ -0,0 +1,11 @@
+package me.tooster.common
+
+infix fun <T> List<T>.cartesianProduct(other: List<T>) =
+    this.flatMap { first -> other.map { second -> first to second } }
+
+infix fun <K, T : Iterable<K>> T.cartesianProduct(other: T) = sequence {
+    val b = other.toList()
+    yieldAll(this@cartesianProduct.flatMap { a -> b.map { b -> a to b }})
+}
+
+fun Pair<Int, Int>.toVec2Int(): Vec2Int = Vec2Int(first, second)
