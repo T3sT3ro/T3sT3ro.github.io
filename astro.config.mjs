659ed78ea5ba591bdc075d943df6a21246348203
Tooster <5300963+T3sT3ro@users.noreply.github.com>
2024-09-27 19:45:44 +0200

    Upgrade package versions, add tailwind and markdoc, configure stylelint tasks

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,22 @@ import sitemap from '@astrojs/sitemap';
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
 
+import markdoc from "@astrojs/markdoc";
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://T3sT3ro.github.io',
   integrations: [mdx(), sitemap({
     lastmod: new Date()
-  }), sentry(), spotlightjs()],
+  }), sentry(), spotlightjs(), markdoc(), tailwind()],
   prefetch: true,
   devToolbar: {
     enabled: true
   },
   experimental: {
-    contentCollectionCache: true
+    contentCollectionCache: true,
+    contentIntellisense: true,
   }
 });
---
+++ markdown.config.mjs
@@ -0,0 +1,11 @@
+import { defineMarkdocConfig } from '@astrojs/markdoc/config';
+import shiki from '@astrojs/markdoc/shiki';
+
+export default defineMarkdocConfig({
+  extends: [
+    shiki({
+      // https://shiki.style/themes
+    //   theme: 'dracula',
+    }),
+  ],
+});
