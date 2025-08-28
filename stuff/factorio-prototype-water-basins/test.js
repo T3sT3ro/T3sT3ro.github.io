842f3fae6edf7e01c795d381f6934bb0db4873b6
Tooster <5300963+T3sT3ro@users.noreply.github.com>
2025-08-29 00:51:11 +0200

    remove redundant test.js file

--- stuff/factorio-prototype-water-basins/test.js
+++
@@ -1,7 +0,0 @@
-// Simple test module
-export const TEST_CONSTANT = "Module loading works!";
-
-export function testFunction() {
-    console.log("Test function called from test.js");
-    return true;
-}
